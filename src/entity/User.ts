9fc3ef2960b026b2eca348548bff323371720416
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-21 12:00:01 -0500

    Define more attributes for Users

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,56 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn} from "typeorm";
+import {IsNotEmpty, Length} from "class-validator";
+import * as bcrypt from "bcryptjs";
+
 
 @Entity()
+@Unique(["username"])
 export class User {
 
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
-    firstName: string;
+    @Length(4, 20)
+    username: string;
 
     @Column()
-    lastName: string;
+    @Length(4, 100)
+    password: string;
+
+    @Column()
+    @IsNotEmpty()
+    role: string;
+
+    @Column()
+    @Length(6, 100)
+    email: string;
 
     @Column()
     age: number;
+
+    @Column()
+    @Length(2, 100)
+    firstName: string;
+
+    @Column()
+    @Length(2, 100)
+    lastName: string;
+
+    @Column()
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Column()
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    hashPassword() {
+        this.password = bcrypt.hashSync(this.password, 8);
+    }
+
+    checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+        return bcrypt.compareSync(unencryptedPassword, this.password);
+    }
 
 }
