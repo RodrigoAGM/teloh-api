d9d36069c57a0c5d4602e66749504cce3625a42b
RodrigoAGM <rod-gm@hotmail.com>
2019-05-27 00:07:11 -0500

    Comment controller implemented

--- src/controller/CommentController.ts
+++ src/controller/CommentController.ts
@@ -1,37 +1,259 @@
 import {Request, Response} from "express";
 import {getRepository} from "typeorm";
 import {Comment} from "../entity/Comment";
+import {User} from "../entity/User";
+import {Room} from "../entity/Room";
+import {validate} from "class-validator";
 
 export class CommentController{
 
     private commentRepository = getRepository(Comment);
 
-    async listAll(req: Request, res: Response){
-        let comments = await this.commentRepository.find();
+    static listAll = async(req: Request, res: Response) => {
+        const commentRepository = getRepository(Comment);
+
+        let comments = await commentRepository.find();
         res.send(comments);
     };
 
-    async getOneById(req: Request, res: Response) {
+    static getOneById = async(req: Request, res: Response) => {
+        const commentRepository = getRepository(Comment);
 
         const id:number = req.params.id;
 
         try {
-            let comment = await this.commentRepository.findOneOrFail(id);
+            let comment = await commentRepository.findOneOrFail(id);
             res.send(comment);
         } catch (error) {
             res.status(404).send("Comment not found");
         }
     };
 
-    async save(req: Request, res: Response){
+    static listAllByUser = async(req: Request, res: Response) => {
+        const commentRepository = getRepository(Comment);
+        const userRepository = getRepository(User);
+
+        let idUser:number = req.params.idUser;
+
+        let user:User;
+        try {
+            user = await userRepository.findOneOrFail(idUser);
+        } catch (error) {
+            res.status(404).send("User not found");
+            return;
+        }
+
+        let comments = await commentRepository.find({where:{user:user}});
+        res.send(comments);
+    };
+
+    static getOneByIdAndUser = async(req: Request, res: Response) => {
+        const commentRepository = getRepository(Comment);
+        const userRepository = getRepository(User);
+
+        const id:number = req.params.id;
+        let idUser:number = req.params.idUser;
+
+        let user:User;
+        try {
+            user = await userRepository.findOneOrFail(idUser);
+        } catch (error) {
+            res.status(404).send("User not found");
+            return;
+        }
+
+        try {
+            let comment = await commentRepository.findOneOrFail({where:{user:user,id:id}});
+            res.send(comment);
+        } catch (error) {
+            res.status(404).send("Comment not found");
+        }
+    };
+
+    static listAllByRoom = async(req: Request, res: Response) => {
+
+        const commentRepository = getRepository(Comment);
+        const roomRepository = getRepository(Room);
+
+        let idRoom:number = req.params.idRoom;
+
+        let room:Room;
+        try {
+            room = await roomRepository.findOneOrFail(idRoom);
+        } catch (error) {
+            res.status(404).send("Room not found");
+            return;
+        }
+
+        let comments = await commentRepository.find({where:{room:room}});
+        res.send(comments);
+    };
+
+    static getOneByIdAndRoom = async(req: Request, res: Response) => {
+
+        const commentRepository = getRepository(Comment);
+        const roomRepository = getRepository(Room);
+
+        const id:number = req.params.id;
+        let idRoom:number = req.params.idRoom;
+
+        let room:Room;
+        try {
+            room = await roomRepository.findOneOrFail(idRoom);
+        } catch (error) {
+            res.status(404).send("Room not found");
+            return;
+        }
+
+        try {
+            let comment = await commentRepository.findOneOrFail({where:{room:room,id:id}});
+            res.send(comment);
+        } catch (error) {
+            res.status(404).send("Comment not found");
+        }
+    };
+
+    static save = async(req: Request, res: Response) => {
+        const commentRepository = getRepository(Comment);
+        const userRepository = getRepository(User);
+        const roomRepository = getRepository(Room);
+
+        let idUser:number = req.params.idUser;
+        let idRoom:number = req.params.idRoom;
+        let {description, published} = req.body;
+
+        let comment = new Comment();
+        comment.description = description;
+        comment.published = published;
+
+        let user:User;
+        try {
+            user = await userRepository.findOneOrFail(idUser);
+        } catch (error) {
+            res.status(404).send("User not found");
+            return;
+        }
+        comment.user = user;
+
+        let room:Room;
+        try {
+            room = await roomRepository.findOneOrFail(idRoom);
+        } catch (error) {
+            res.status(404).send("Room not found");
+            return;
+        }
+        comment.room = room;
+
+
+        const errors = await validate(Comment);
+        if (errors.length > 0) {
+            res.status(400).send(errors);
+            return;
+        }
+
+        try {
+            await commentRepository.save(comment);
+        }catch (errors){
+            res.status(409).send("Something went wrong");
+            return;
+        }
+
+        res.status(201).send("Comment saved");
+    };
+
+    static edit = async(req: Request, res: Response) => {
+
+        const commentRepository = getRepository(Comment);
+        const userRepository = getRepository(User);
+        const roomRepository = getRepository(Room);
+
+        let idUser:number = req.params.idUser;
+        let idRoom:number = req.params.idRoom;
+        let id:number = req.params.id;
+        let {description, published} = req.body;
+
+        let user:User;
+        try {
+            user = await userRepository.findOneOrFail(idUser);
+        } catch (error) {
+            res.status(404).send("User not found");
+            return;
+        }
+
+        let room:Room;
+        try {
+            room = await roomRepository.findOneOrFail(idRoom);
+        } catch (error) {
+            res.status(404).send("Room not found");
+            return;
+        }
+
+        let comment:Comment;
+        try {
+            comment = await commentRepository.findOneOrFail(id);
+        } catch (error) {
+            res.status(404).send("Comment not found");
+            return;
+        }
+
+        comment.description = description;
+        comment.published = published;
+        comment.user = user;
+        comment.room = room;
+
+        const errors = await validate(comment);
+        if (errors.length > 0) {
+            res.status(400).send(errors);
+            return;
+        }
+
+        try {
+            await commentRepository.save(comment);
+        }catch (errors){
+            res.status(409).send("Something went wrong");
+            return;
+        }
+
+        res.status(201).send("Comment edited");
 
     };
 
-    async edit(req: Request, res: Response) {
+    static delete = async(req: Request, res: Response) => {
 
-    };
+        const commentRepository = getRepository(Comment);
+        const userRepository = getRepository(User);
+        const roomRepository = getRepository(Room);
 
-    async delete(req: Request, res: Response) {
+        let idUser:number = req.params.idUser;
+        let idRoom:number = req.params.idRoom;
+        let id:number = req.params.id;
 
+        let user:User;
+        try {
+            user = await userRepository.findOneOrFail(idUser);
+        } catch (error) {
+            res.status(404).send("User not found");
+            return;
+        }
+
+        let room:Room;
+        try {
+            room = await roomRepository.findOneOrFail(idRoom);
+        } catch (error) {
+            res.status(404).send("Room not found");
+            return;
+        }
+
+        let comment:Comment;
+        try {
+            comment = await commentRepository.findOneOrFail(id);
+        } catch (error) {
+            res.status(404).send("Comment not found");
+            return;
+        }
+
+        commentRepository.delete(id);
+
+        res.status(205).send("Comment deleted successfully");
     };
 }
